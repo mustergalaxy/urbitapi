887821cfcebd667c2ae0d401d294c7326e2889a6
manishweb23 <manish@web23.io>
2024-03-11 17:34:19 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const getDomainsFromENS = require('./getDomainsFromENS');
+
+// Route to get a specific user by ID
+app.get('/address/:ensName', async (req, res) => {
+  try {
+    const { ensName } = req.params;
+    const address = await getDomainsFromENS.resolve(ensName);
+    res.json({ address });
+  } catch (error) {
+    console.error('Error resolving ENS name:', error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
